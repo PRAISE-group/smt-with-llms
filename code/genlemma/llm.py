d8faa4e3b12316b4231995047f5a35210f08d0c6
lahiri-phdworks <lahiri.phdworks@gmail.com>
2025-05-20 11:21:31 +0530

    Final Draft

--- code/genlemma/llm.py
+++ code/genlemma/llm.py
@@ -1,4 +1,25 @@
 # TODO: Sumit
+from typing import List, Optional
+from code.models import Function, Lemmas, LemmaStatus, AlgoVerdict
+from code.genlemma.promptTemplates import *
 
-def genLemma(args):
-    return ["(assert(forall ((x Int) (y Int))     (= (foo_cb x y) (+ x y))    ))"]
+def initPrompt():
+    pass
+
+def generateLemmas(func: Function) -> List[Lemmas]:
+    """
+    Descp: Take in a input of type Function and return a list of Lemmas
+    We use the prompts as shown in code.genlemma.promptTemplates
+    Lemmas added here will have UNKNOWN status
+    """
+    return []
+
+def refineLemmas(lemmas: List[Lemmas]) -> List[Lemmas]:
+    """
+    Descp: Take in a list of Lemmas that have INVALID lemma status
+    and return a list of Lemmas after LLM refinement
+    We will possibly create a new lemmas.
+    Lemmas added here will have UNKNOWN status
+    """
+    return []
+
