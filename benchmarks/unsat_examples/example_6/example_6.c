680e10141edbb5729e49acb7c686c11e77e9a45a
lahiri-phdworks <lahiri.phdworks@gmail.com>
2025-10-10 15:53:23 +0530

    OOPSLA Submission

--- benchmarks/unsat_examples/example_6/example_6.c
+++ benchmarks/unsat_examples/example_6/example_6.c
@@ -5,7 +5,7 @@
 
 long long binpow_cb (long long a, long long b) {
     if (b == 0) return 1;
-    long long res = binpow(a, b / 2);
+    long long res = binpow_cb (a, b / 2);
     if (b % 2)
         return res * res * a;
     else
