64416a73e696878b7cfcef6ae7ee8e67ba27e7b7
tgourav <tgourav@cse.iitk.ac.in>
2025-10-29 05:38:59 +0000

    z3-solver version change

--- parallel.sh
+++ parallel.sh
@@ -69,27 +69,18 @@ kill_tree() {
 # Run program2 in its own process group with timeout
 cd /home/
 # Done
-# model="--model gpt-oss:20b --use156"
-# Remaining
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --model llama3:latest >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --usebedrock --model meta.llama4-maverick-17b-instruct-v1:0 >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --usebedrock --model qwen.qwen3-coder-30b-a3b-v1:0 >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --usebedrock --model qwen.qwen3-32b-v1:0 >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --usebedrock --model us.anthropic.claude-sonnet-4-20250514-v1:0 >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --model llama3:latest >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 -v --model llama3:latest >> "$3" 2>&1 &
-# setsid timeout 10m uv run main.py -i "$1" -t 1 --usebedrock --model openai.gpt-oss-120b-1:0 >> "$3" 2>&1 &
-model="--usebedrock --model meta.llama4-maverick-17b-instruct-v1:0"
-model="--usebedrock --model qwen.qwen3-coder-30b-a3b-v1:0"
-model="--usebedrock --model qwen.qwen3-32b-v1:0"
-model="--usebedrock --model us.anthropic.claude-sonnet-4-20250514-v1:0"
+model="--model gpt-oss:20b --use156"
+# model="--usebedrock --model meta.llama4-maverick-17b-instruct-v1:0"
+# model="--usebedrock --model qwen.qwen3-coder-30b-a3b-v1:0"
+# model="--usebedrock --model qwen.qwen3-32b-v1:0"
+# model="--usebedrock --model us.anthropic.claude-sonnet-4-20250514-v1:0"
 # model="--model llama3:latest"
-model="--usebedrock --model openai.gpt-oss-120b-1:0"
+# model="--usebedrock --model openai.gpt-oss-120b-1:0"
 # for i in {1..5}; do
     # if ! check_files; then
         # echo "not sat checking for unsat"
         echo "Running: uv run main.py -i $1 -t 1 -v $model" > "$3" 2>&1
-        setsid timeout 3m uv run main.py -i "$1" -t 1 -v $model >> "$3" 2>&1 &
+        setsid timeout 10m uv run main.py -i "$1" -t 1 -v $model >> "$3" 2>&1 &
         pid2=$!
         exit_code=$?
         if ! wait "$pid2"; then
